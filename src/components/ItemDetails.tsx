b9e2b7c107a0d3d165c8db5900caef11fec79405
Swag-blips <coderblip@gmail.com>
2024-11-19 11:49:22 +0100

    feat:update

--- src/components/ItemDetails.tsx
+++ src/components/ItemDetails.tsx
@@ -9,8 +9,8 @@ const ItemDetails = ({ items, total }: Props) => {
   return (
     <>
       <div className="w-full rounded-r-[8px] rounded-l-[8px] mt-12 p-8 bg-[#F9FAFE]">
-        <div className="grid grid-cols-2">
-          <div className=" flex flex-col gap-8">
+        <div className="md:grid md:grid-cols-2 flex items-center justify-between">
+          <div className=" flex flex-col gap-6 md:gap-8">
             <p className="text-[#7E88C3] hidden md:block">Item Name</p>
 
             {items?.map((item) => (
@@ -42,8 +42,8 @@ const ItemDetails = ({ items, total }: Props) => {
                 </p>
               ))}
             </div>
-            <div className=" flex flex-col items-center gap-8">
-              <h1 className="text-[16px] text-[#7E88C3] font-medium tracking=[-0.1px] text-left">
+            <div className=" flex flex-col items-center justify-center gap-6 md:gap-8">
+              <h1 className="text-[16px] hidden md:inline text-[#7E88C3] font-medium tracking=[-0.1px] text-left">
                 Price
               </h1>
 
@@ -80,7 +80,6 @@ const ItemDetails = ({ items, total }: Props) => {
           £ {total}
         </p>
       </div>
-
     </>
   );
 };
