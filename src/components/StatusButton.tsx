4356eb1a5c239fa95a79501a4c8e963048cbfe99
Swag-blips <coderblip@gmail.com>
2024-11-20 11:09:37 +0100

    fix:draft-button bug

--- src/components/StatusButton.tsx
+++ src/components/StatusButton.tsx
@@ -11,16 +11,16 @@ const StatusButton = ({ invoice }: Props) => {
           ? "bg-paid-status"
           : invoice?.status === "pending"
           ? "bg-pending-status"
-          : "bg-draft-status"
+          : "bg-draft-status dark:bg-[#DFE3FA] dark:bg-opacity-[5.71%] "
       } rounded-md bg-opacity-[5.71%] h-10`}
-    >             
-      <div   
+    >
+      <div
         className={`w-2 h-2  ${
           invoice?.status === "paid"
             ? "bg-paid-status"
             : invoice?.status === "pending"
             ? "bg-pending-status"
-            : "bg-draft-status"
+            : "bg-draft-status dark:bg-[#DFE3FA] "
         } rounded-full`}
       />
       <p
@@ -29,7 +29,7 @@ const StatusButton = ({ invoice }: Props) => {
             ? "text-paid-status"
             : invoice?.status === "pending"
             ? "text-pending-status"
-            : "text-draft-status"
+            : "text-draft-status dark:text-[#DFE3FA]"
         } font-bold tracking-tight opacity-100`}
       >
         {invoice?.status}
